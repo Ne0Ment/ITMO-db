7728c578f5dbd9a9b6b262f2a24d74833d0b787f
NeoMent <16ilyamikheev@gmail.com>
2023-03-15 21:35:14 +0300

    fixed drop_all

--- lab1/drop_all.sql
+++ lab1/drop_all.sql
@@ -1,12 +1,12 @@
 DROP TABLE feelingcharacteristics, friendship, observings, people, senses, feelings, ability, knows CASCADE;
 
-DO $$ DECLARE
-    tabname RECORD;
-BEGIN
-    FOR tabname IN (SELECT tablename 
-                    FROM pg_tables 
-                    WHERE schemaname = current_schema()) 
-LOOP
-    EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(tabname.tablename) || ' CASCADE';
-END LOOP;
-END $$;
+-- DO $$ DECLARE
+--     tabname RECORD;
+-- BEGIN
+--     FOR tabname IN (SELECT tablename 
+--                     FROM pg_tables 
+--                     WHERE schemaname = current_schema()) 
+-- LOOP
+--     EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(tabname.tablename) || ' CASCADE';
+-- END LOOP;
+-- END $$;
