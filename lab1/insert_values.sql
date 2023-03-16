f9926d242af9f53daf5f8c0ec142818c6ea0ab56
NeoMent <16ilyamikheev@gmail.com>
2023-03-16 08:37:06 +0300

    added more data for query

--- lab1/insert_values.sql
+++ lab1/insert_values.sql
@@ -7,4 +7,5 @@ INSERT INTO Knows (ability_id, person_id) VALUES (1, 2);
 INSERT INTO Observings (observer_id, subject_id) VALUES (2, 1);
 INSERT INTO Friendship (person1_id, person2_id) VALUES (2, 1);
 INSERT INTO Senses (person_id, feeling_id) VALUES (1, 1);
+INSERT INTO Senses (person_id, feeling_id) VALUES (2, 1);
 INSERT INTO FeelingCharacteristics (characteristic_id, feeling_id, characteristic_name) VALUES (DEFAULT, 1, 'приятное');
